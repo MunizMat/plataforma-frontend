05fd503355ff91f3b0760714cc9db136a9986a66
Matheus Muniz <103901768+MunizMat@users.noreply.github.com>
2023-04-07 13:28:14 -0300

    Alterando componente de formulário de login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react";
 import { Link } from 'react-router-dom';
-// import { emailIsValid, fieldIsEmpty } from "@modules/formValidation";
+import { validateEmail, fieldIsEmpty} from "@modules/formValidation";
 import './index.css';
 //Bootstrap 
 import Form from 'react-bootstrap/Form';
@@ -9,40 +9,55 @@ import Container from 'react-bootstrap/Container';
 
 
 export default function Login () {
-    const [validated, setValidated] = useState(false);
+    // Estados de verificação da veracidade dos campos
+    const [emailInvalidity, setEmailInvalidity] = useState(false);
+    const [passwordInvalidity, setPasswordInvalidity] = useState(false);
     const [emailErrorMessage, setEmailErrorMessage] = useState('Este campo é obrigatório');
     const emailRef = useRef();
+    const passwordRef = useRef();
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        const form = event.currentTarget;
 
-        if(!fieldIsEmpty(emailRef.current.value) && !emailIsValid(emailRef.current.value)) {
-            setEmailErrorMessage('Email inválido');
-        } 
+         /* Executando funções de validação de campo. A funçao "validate<nomeDoCampo>" retorna um objeto que informa se o campo é valido, e, caso seja inválido, qual mensagem de erro deve ser exibida */
+         const { emailIsValid, emailErrorMessage } = validateEmail(emailRef.current.value);
+ 
+         // Sequência de validação individual de cada campo 
+         if (!emailIsValid) {
+             setEmailErrorMessage(emailErrorMessage);
+             setEmailInvalidity(true);
+         } else {
+             setEmailInvalidity(false);
+         }
 
-        setValidated(true);
+        if (fieldIsEmpty(passwordRef.current.value)){
+            setPasswordInvalidity(true);
+        } else {
+            setPasswordInvalidity(false);
+        }
+ 
 
-        if(form.checkValidity()) {
+        if(emailIsValid && !(fieldIsEmpty(passwordRef.current.value))) {
+            const form = event.currentTarget;
             form.submit();
         }
     }
   
     return(
         <Container style={{height: "80%"}} className="d-flex justify-content-center align-items-center">
-                <Form noValidate validated={validated} onSubmit={handleSubmit} className="form bg-light p-3 ps-4 rounded" style={{ width: "450px", textAlign: "left"}}>
+                <Form noValidate onSubmit={handleSubmit} className="form bg-light p-3 ps-4 rounded" style={{ width: "450px", textAlign: "left"}}>
 
                     <h2>Fazer login</h2>
 
                     <Form.Group className="mb-3" controlId="formBasicEmail" style={{textAlign: "left"}} >
                         <Form.Label>Seu Email</Form.Label>
-                        <Form.Control required ref={emailRef} type="email" placeholder="Email" />
+                        <Form.Control isInvalid={emailInvalidity} required ref={emailRef} type="email" placeholder="Email" />
                         <Form.Control.Feedback type="invalid">{emailErrorMessage}</Form.Control.Feedback>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword" style={{textAlign: "left"}} >
                         <Form.Label>Sua Senha</Form.Label>
-                        <Form.Control required type="password" placeholder="Senha" />
+                        <Form.Control isInvalid={passwordInvalidity} required ref={passwordRef} type="password" placeholder="Senha" />
                         <Form.Control.Feedback type="invalid">Este campo é obrigatório</Form.Control.Feedback>
                     </Form.Group>
 
