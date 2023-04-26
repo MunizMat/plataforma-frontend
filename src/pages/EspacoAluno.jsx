3ded122799130c6acd76f6a871a3bac72f7cbd28
Matheus Muniz <103901768+MunizMat@users.noreply.github.com>
2023-04-26 00:35:50 -0300

    Adicionando efeito de fade

--- src/pages/EspacoAluno.jsx
+++ src/pages/EspacoAluno.jsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import { useSelector } from "react-redux";
 // Bootstrap
 import Container from 'react-bootstrap/Container';
 import Stack from 'react-bootstrap/Stack';
 import Col from 'react-bootstrap/Col';
+import Fade from 'react-bootstrap/Fade';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import { useSelector } from "react-redux";
 
 export default function EspacoAluno () {
     const user = useSelector(state => state.auth.user);
-    console.log(user);
+    const [show, setShow] = useState(false);
 
+    useEffect(() => {
+        setTimeout(() => {
+            setShow(true);
+        }, 500);
+    }, []);
+  
     return(
         <Stack gap={5}>
-            <h1 className="display-1">Olá, {user.nome}</h1>
+            <Fade in={show} appear  timeout={1000} >
+                <h1 id="example-fade-text" className="display-1">Olá, {user.nome}</h1>
+            </Fade>
             <Container style={{ width: '50%', color: 'black'}} className="bg-light rounded p-3" >
                 <h2>Avaliações</h2>
                 <Button as='a' href="/avaliacao">Realizar nova avaliação</Button>
